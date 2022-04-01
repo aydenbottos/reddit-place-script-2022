15e827309f2e25b38166904d29a85ca55fce17fd
DeltaNeverUsed <sinus06@hotmail.com>
2022-04-02 01:15:26 +0200

    third time's the charm

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def color_id_to_name(color_id):
 def closest_color(target_rgb, rgb_colors_array_in):
     r, g, b, a = target_rgb
     print(r,g,b,a)
-    if a < 255:
+    if a < 255 or (r,g,b) == (69,42,0):
         return (69,42,0)
     color_diffs = []
     for color in rgb_colors_array_in:
@@ -297,7 +297,8 @@ def get_unset_pixel(img):
         target_rgb = pix[x, y]
         new_rgb = closest_color(target_rgb, rgb_colors_array)
         if pix2[x,y] != new_rgb:
-            if new_rgb != (69,42,0,255):
+            print(new_rgb,new_rgb != (69,42,0))
+            if new_rgb != (69,42,0):
                 print("Different Pixel found at:",x+pixel_x_start,y+pixel_y_start,"With Color:",pix2[x+pixel_x_start,y+pixel_y_start],"Replacing with:",new_rgb)
                 break;
             else:
@@ -444,7 +445,7 @@ while True:
         print("received new access token: ", access_token)
 
     # draw pixel onto screen
-    if access_token is not None and current_timestamp >= last_time_placed_pixel + pixel_place_frequency:
+    if True: #access_token is not None and current_timestamp >= last_time_placed_pixel + pixel_place_frequency:
         # get current pixel position from input image
         r, c = get_unset_pixel(get_board(access_token))
 
